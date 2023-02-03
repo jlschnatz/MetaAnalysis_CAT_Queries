8f8d3ab3ec1608184d25513a1028a79e38dd34b7
Schnatz <janluca.schnatz@gmx.de>
2023-02-03 22:41:06 +0100

    cleanup

--- R/find_duplicates.R
+++ R/find_duplicates.R
@@ -1,5 +1,4 @@
 # Load packages and plan parallel multisession ----
-
 if (!"pacman" %in% installed.packages()) {
   install.packages("pacman")
 }
@@ -8,9 +7,7 @@ pacman::p_load(tidyverse, here, revtools, fs, furrr, lubridate)
 ms_cores <- availableCores() - 2
 plan(multisession, workers = ms_cores)
 
-list.files("data", recursive = TRUE, full.names = TRUE) %>%
-  file_info() %>% view()
-
+# Filter files for newest searches (most updated) ----
 newest_search_path <- dir_ls(
   path = here("data"),
   recurse = TRUE,
@@ -21,70 +18,27 @@ newest_search_path <- dir_ls(
   mutate(date = ymd(str_extract(filepath, "[0-9]{6}"))) %>%
   slice_max(date, by = db_source)
 
-
-
-<# Deduplication all files combined -----
-
-df_combined_raw <- dir_ls(
-  path = here("data"),
-  recurse = TRUE,
-  regexp = "(\\.ris$)|(\\.nbib$)"
-) %>%
+# Deduplication all files combined -----
+df_combined_raw <- newest_search_path %>%
+  pull(filepath) %>%
   future_map_dfr(read_bibliography, .id = "id_path")
 
 find_dups <- df_combined_raw %>%
-  find_duplicates()
+  find_duplicates(match_variable = "title", to_lower = TRUE)
+
+find_dups %>%
+  as.integer() %>%
+  duplicated() %>%
+  sum() # 185 Duplicates
 
 df_combined_clean <- extract_unique_references(
-  x = df_combined_raw, 
+  x = df_combined_raw,
   matches = find_dups
-  )
+)
 
 # save to .ris
 write_bibliography(
   x = df_combined_clean,
-  filename = here("data/complete_cleaned_query.ris"),
+  filename = here("data/db_dedup_query.ris"),
   format = "ris"
-  )
-
-# Count entries per file ----
-
-list_bib <- dir_ls(
-  path = here("data"),
-  recurse = TRUE,
-  regexp = "(\\.ris$)|(\\.nbib$)"
-) %>%
-  future_map(read_bibliography)
-
-df_bib <- list_bib %>%
-  future_map(
-    ~find_duplicates(.x, to_lower = TRUE) %>%
-      as.integer() %>%
-      as_tibble() 
-  ) %>%
-  enframe(name = "id_path", value = "id_entry") %>% 
-  unnest(id_entry) %>%
-  rename(id_entry = value) %>%
-  mutate(id_path = basename(id_path)) 
-
-
-count_database_entries <- df_bib %>%
-  group_by(id_path) %>%
-  count() %>% 
-  ungroup()
-
-duplicates_sum <- df_bib %>%
-  group_by(id_path) %>%
-  summarise(n = sum(duplicated(id_entry)))
-
-duplicates_ids <- df_ris %>%
-  group_by(id_path) %>%
-  mutate(is_duplicated = duplicated(id_entry)) %>% 
-  filter(is_duplicated)  %>%
-  ungroup()
-
-
-
-
-
-
+)
